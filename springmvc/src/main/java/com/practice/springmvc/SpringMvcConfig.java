84a78f444e9f1503e633923177d74199126818ee
avinash10592 <59564861+avinash10592@users.noreply.github.com>
2020-04-02 23:06:52 +0530

    Add files via upload

---
+++ springmvc/src/main/java/com/practice/springmvc/SpringMvcConfig.java
@@ -0,0 +1,21 @@
+package com.practice.springmvc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan({ "com.practice.springmvc" })
+public class SpringMvcConfig {
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver vr = new InternalResourceViewResolver();
+		vr.setPrefix("/WEB-INF/");
+		vr.setSuffix(".jsp");
+
+		return vr;
+	}
+
+}
